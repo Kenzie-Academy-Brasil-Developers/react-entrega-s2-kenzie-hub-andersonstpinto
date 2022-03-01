a57b9fc6fcb2b781c8d372fc4237f5185b0269f4
Anderson Pinto <74440689+andersonstpinto@users.noreply.github.com>
2022-03-01 14:05:22 -0300

    style: global reset

---
+++ src/styles/global.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    outline: 0;
+    font-family: 'Inter', sans-serif;
+  }
+
+  :root {
+    --primary: #ff577f;
+    --primary-focus: #ff427f;
+    --primary-negative: #59323f;
+    --gray-4: #121214;
+    --gray-3: #212529;
+    --gray-2: #343b41;
+    --gray-1: #868e96;
+    --gray-0: #f8f9fa;
+    --success: #3fe864;
+    --failure: #e83f5b;
+  }
+`
